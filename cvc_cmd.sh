c2c2de14d3b24775cb34cb804353fee5257b56fe
Abdul Dakkak <abduld@wolfram.com>
2015-04-24 19:56:29 -0500

    work on getting profiling to work

--- cvc_cmd.sh
+++ cvc_cmd.sh
@@ -5,8 +5,8 @@ dir=`pwd`
 export OMP_PROC_BIND=true
 
 ###define pachi commands##
-white="${dir}/code/pachi -e montecarlo -t _10 -T 12"
-black="${dir}/code/pachi -e montecarlo -t _10 -T 12"
+white="${dir}/code/pachi -e montecarlo -t _1 -T 8"
+black="${dir}/code/pachi -e montecarlo -t _1 -T 8"
 
 ##remove outputfiles from previous run##
 #rm test.summary.dat
@@ -18,7 +18,7 @@ goguitwogtp="${dir}/gogui/bin/gogui-twogtp"
 
 ## play multiple games##
 games=1
-${goguitwogtp} -black "${black}" -white "${white}" -size 9 -auto -games ${games} -sgffile test -time 10 -verbose >gtg_output.txt 2>&1
+${goguitwogtp} -black "${black}" -white "${white}" -size 7 -auto -games ${games} -sgffile test -time 10 -verbose >gtg_output.txt 2>&1
 
 ##generate human readable html output file##
 ${goguitwogtp} -analyze test.dat
