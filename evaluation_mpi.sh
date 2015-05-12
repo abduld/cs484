33fd1a4e1efdb7cc79c92529f0ebc2c4f1854484
Ubuntu <ubuntu@ip-172-31-5-148.ec2.internal>
2015-05-12 17:55:38 +0000

    eval mod

--- evaluation_mpi.sh
+++ evaluation_mpi.sh
@@ -1,9 +1,37 @@
 dir=`pwd`
 goguitwogtp="${dir}/gogui/bin/gogui-twogtp"
 
-for baseline_threads in 1 2 4 8
+for baseline_threads in 32
 do
-   for mpi_threads in 2 4 8 12
+   for mpi_threads in 2 4
+   do
+      if [ ${mpi_threads} -gt ${baseline_threads} ]
+      then
+	###define pachi commands##
+	white="mpiexec -np ${mpi_threads} ${dir}/code/pachi -e montecarlo_mpi -t _10 -T 1"
+	if [ ${baseline_threads} -eq 1 ]
+	then
+		black="${dir}/code/pachi -e montecarlo_original -t _10"
+	else
+		black="mpiexec -np ${baseline_threads} ${dir}/code/pachi -e montecarlo_mpi -t _10 -T 1"
+	fi
+	for trial in 1 2
+	do
+		games=5
+		${goguitwogtp} -black "${black}" -white "${white}" -size 9 -auto -games ${games} -sgffile test -time 10 -verbose >gtg_output.txt 2>&1
+		${goguitwogtp} -analyze test.dat
+		my_test="MPI_${mpi_threads}_vs_${baseline_threads}_${trial}"
+		mkdir ${my_test}
+		mv gtg_output.txt ./${my_test}/gtg_output.txt
+		mv test* ./${my_test}/
+	done
+      fi
+   done
+done
+
+for baseline_threads in 1 2 4 8 16 24
+do
+   for mpi_threads in 2 4 8 12 
    do
       if [ ${mpi_threads} -gt ${baseline_threads} ]
       then
--- evaluation_omp.sh
+++ evaluation_omp.sh
@@ -1,24 +1,74 @@
 dir=`pwd`
 goguitwogtp="${dir}/gogui/bin/gogui-twogtp"
 
-for baseline_threads in 1 2 4 8 16
+threads=35
+baseline_threads=1
+if [ ${threads} -gt ${baseline_threads} ]
+then
+###define pachi commands##
+white="${dir}/code/pachi -e montecarlo -t _5 -T ${threads}"
+if [ ${baseline_threads} -eq 1 ]
+then
+	black="${dir}/code/pachi -e montecarlo_original -t _5"
+else
+	black="${dir}/code/pachi -e montecarlo -t _5 -T ${baseline_threads}"
+fi
+for trial in 1 2
 do
-   for threads in 2 4 8 12
+	games=5
+	${goguitwogtp} -black "${black}" -white "${white}" -size 19 -auto -games ${games} -sgffile test -time 5 -verbose >gtg_output.txt 2>&1
+	${goguitwogtp} -analyze test.dat
+	my_test="OMP_${threads}_vs_${baseline_threads}_${trial}"
+	mkdir ${my_test}
+	mv gtg_output.txt ./${my_test}/gtg_output.txt
+	mv test* ./${my_test}/
+done
+fi
+
+for baseline_threads in 2 4
+threads=32
+do
+if [ ${threads} -gt ${baseline_threads} ]
+then
+###define pachi commands##
+white="${dir}/code/pachi -e montecarlo -t _5 -T ${threads}"
+if [ ${baseline_threads} -eq 1 ]
+then
+	black="${dir}/code/pachi -e montecarlo_original -t _5"
+else
+	black="${dir}/code/pachi -e montecarlo -t _5 -T ${baseline_threads}"
+fi
+for trial in 1 2
+do
+	games=5
+	${goguitwogtp} -black "${black}" -white "${white}" -size 19 -auto -games ${games} -sgffile test -time 5 -verbose >gtg_output.txt 2>&1
+	${goguitwogtp} -analyze test.dat
+	my_test="OMP_${threads}_vs_${baseline_threads}_${trial}"
+	mkdir ${my_test}
+	mv gtg_output.txt ./${my_test}/gtg_output.txt
+	mv test* ./${my_test}/
+done
+fi
+done
+
+for threads in 1 2 4 8 16 24
+do
+   for baseline_threads in 2 4 8 12 16
    do
       if [ ${threads} -gt ${baseline_threads} ]
       then
 	###define pachi commands##
-	white="${dir}/code/pachi -e montecarlo -t _10 -T ${threads}"
+	white="${dir}/code/pachi -e montecarlo -t _5 -T ${threads}"
 	if [ ${baseline_threads} -eq 1 ]
 	then
-		black="${dir}/code/pachi -e montecarlo_original -t _10"
+		black="${dir}/code/pachi -e montecarlo_original -t _5"
 	else
-		black="${dir}/code/pachi -e montecarlo -t _10 -T ${baseline_threads}"
+		black="${dir}/code/pachi -e montecarlo -t _5 -T ${baseline_threads}"
 	fi
 	for trial in 1 2
 	do
 		games=5
-		${goguitwogtp} -black "${black}" -white "${white}" -size 19 -auto -games ${games} -sgffile test -time 10 -verbose >gtg_output.txt 2>&1
+		${goguitwogtp} -black "${black}" -white "${white}" -size 19 -auto -games ${games} -sgffile test -time 5 -verbose >gtg_output.txt 2>&1
 		${goguitwogtp} -analyze test.dat
 		my_test="OMP_${threads}_vs_${baseline_threads}_${trial}"
 		mkdir ${my_test}
