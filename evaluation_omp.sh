ca11ee97ec5f7de57c3f64623d0542f45687a924
Ubuntu <ubuntu@ip-172-31-5-148.ec2.internal>
2015-05-13 00:33:41 +0000

    1

--- evaluation_omp.sh
+++ evaluation_omp.sh
@@ -1,33 +1,9 @@
 dir=`pwd`
 goguitwogtp="${dir}/gogui/bin/gogui-twogtp"
 
-threads=35
-baseline_threads=1
-if [ ${threads} -gt ${baseline_threads} ]
-then
-###define pachi commands##
-white="${dir}/code/pachi -e montecarlo -t _5 -T ${threads}"
-if [ ${baseline_threads} -eq 1 ]
-then
-	black="${dir}/code/pachi -e montecarlo_original -t _5"
-else
-	black="${dir}/code/pachi -e montecarlo -t _5 -T ${baseline_threads}"
-fi
-for trial in 1 2
-do
-	games=5
-	${goguitwogtp} -black "${black}" -white "${white}" -size 19 -auto -games ${games} -sgffile test -time 5 -verbose >gtg_output.txt 2>&1
-	${goguitwogtp} -analyze test.dat
-	my_test="OMP_${threads}_vs_${baseline_threads}_${trial}"
-	mkdir ${my_test}
-	mv gtg_output.txt ./${my_test}/gtg_output.txt
-	mv test* ./${my_test}/
-done
-fi
-
 for baseline_threads in 2 4
-threads=32
 do
+threads=32
 if [ ${threads} -gt ${baseline_threads} ]
 then
 ###define pachi commands##
